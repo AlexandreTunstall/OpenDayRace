9628e2b68a66b93f8e2a99a2a7b88b80d5c428a4
Alex Tunstall <alexandrejamestunstall@gmail.com>
2020-02-09 11:36:55 +0000

    Start adding client-side networking logic

---
+++ src/main/java/uk/ac/exeter/opendayrace/client/io/ServerConnection.java
@@ -0,0 +1,49 @@
+package uk.ac.exeter.opendayrace.client.io;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import static uk.ac.exeter.opendayrace.common.io.NetworkConstants.*;
+
+public class ServerConnection {
+    // Must be large enough to store all the bytes that will be handled at the same time
+    private static final int BUFFER_SIZE = 4096;
+
+    private final SocketChannel socket;
+    private final ByteBuffer buffer;
+
+    public ServerConnection(InetSocketAddress address) throws IOException {
+        socket = SocketChannel.open(address);
+        buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+        buffer.put(VERSION);
+        buffer.put(VERSION_END);
+        writeBuffer();
+        readStatus();
+    }
+
+    private void readStatus() throws IOException {
+        buffer.limit(1);
+        readBuffer();
+        byte status = buffer.get();
+        if (status < 0) {
+            throw new IOException("Server sent failure status code: " + status);
+        }
+        // TODO Set state based on status code
+    }
+
+    private void writeBuffer() throws IOException {
+        while (buffer.position() < buffer.limit()) {
+            socket.write(buffer);
+        }
+        buffer.clear();
+    }
+
+    private void readBuffer() throws IOException {
+        while (buffer.position() < buffer.limit()) {
+            socket.read(buffer);
+        }
+        buffer.flip();
+    }
+}
